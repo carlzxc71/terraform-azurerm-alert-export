ceaca20f6b0aea577b937c95e4d9b716265fad5b
Carl Lindberg <carl.lindberg@iver.se>
2023-04-20 11:23:09 +0200

    spelling mistak

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "alert_export" {
   source = "./modules"
 
   rg_name         = "rg-alertexport-p-weu-001"
-  aa_account_name = "aa-alertexport-p-wue-001"
+  aa_account_name = "aa-alertexport-p-weu-001"
 }
