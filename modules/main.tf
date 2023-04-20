9d91543a07130309703af7623a85fbe8cedf9d23
Carl Lindberg <carl.lindberg@iver.se>
2023-04-20 13:12:55 +0200

    added variables

--- modules/main.tf
+++ modules/main.tf
@@ -53,3 +53,21 @@ resource "azurerm_automation_job_schedule" "this" {
   automation_account_name = azurerm_automation_account.this.name
   runbook_name            = azurerm_automation_runbook.this.name
 }
+
+resource "azurerm_automation_variable_string" "secret" {
+  name                    = "accountKey"
+  resource_group_name     = azurerm_resource_group.this.name
+  automation_account_name = azurerm_automation_account.this.name
+  value                   = "Temp Placeholder"
+  encrypted               = true
+  description             = "Account key for access to the storage account"
+}
+
+resource "azurerm_automation_variable_string" "secret" {
+  name                    = "storageAccountName"
+  resource_group_name     = azurerm_resource_group.this.name
+  automation_account_name = azurerm_automation_account.this.name
+  value                   = "Temp placeholder"
+  encrypted               = true
+  description             = "Account key for access to the storage account"
+}
