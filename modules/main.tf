662bdf16db08ba7d1af78c0e7bef53c7f6f2f3e1
Carl Lindberg <carl.lindberg@iver.se>
2023-04-21 08:34:13 +0200

    removed encryption from storage account variable

--- modules/main.tf
+++ modules/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_automation_variable_string" "storage_acc_name" {
   resource_group_name     = azurerm_resource_group.this.name
   automation_account_name = azurerm_automation_account.this.name
   value                   = "Temp placeholder"
-  encrypted               = true
+  encrypted               = false
   description             = "Account key for access to the storage account"
 }
 
